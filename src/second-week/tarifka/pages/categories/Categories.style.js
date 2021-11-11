cf083efac8fa903c01385a44144acd032b2c0d84
Mesut ÖZTÜRK <mesut8311006@gmail.com>
2021-11-11 21:25:05 +0300

    added category styles

---
+++ src/second-week/tarifka/pages/categories/Categories.style.js
@@ -0,0 +1,25 @@
+import { StyleSheet, Dimensions } from 'react-native'
+
+
+export default StyleSheet.create({
+    container: {
+        // paddingBottom:50,
+    },
+    top_container:{
+        // flex:1,
+        flexDirection: 'row',
+    },
+    header_text:{
+        color:'#FFA500',
+        fontSize:20,
+        textAlign:'center',
+        marginBottom:10,
+        flex: 1,
+    },
+    header_button:{},
+    list: {
+        backgroundColor:'#FFA500',
+        padding:10,
+    },
+
+})
