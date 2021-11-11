030c81814bc33ba56c1910e16fc6d19f5488a01d
Mesut ÖZTÜRK <mesut8311006@gmail.com>
2021-11-11 21:25:54 +0300

    added meal style

---
+++ src/second-week/tarifka/pages/meals/Meals.style.js
@@ -0,0 +1,26 @@
+import { StyleSheet, Dimensions } from 'react-native'
+
+
+export default StyleSheet.create({
+    container: {
+        // padding:10,
+    },
+    top_container:{
+        // flex:1,
+        flexDirection: 'row',
+        padding:10,
+    },
+    header_text:{
+        color:'#FFA500',
+        fontSize:20,
+        textAlign:'center',
+        marginBottom:10,
+        flex: 1,
+    },
+    header_button:{},
+    list: {
+        backgroundColor:'#FFA500',
+        padding:10,
+    },
+
+})
